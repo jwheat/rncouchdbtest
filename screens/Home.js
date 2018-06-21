1ed70f83ac2601dbf3960bee1222fe27c196f21a
Jonathan Wheat <jonathan.p.wheat@gmail.com>
2018-06-21 10:13:05 -0400

    screwed up the readme, awesome

--- screens/Home.js
+++ screens/Home.js
@@ -20,7 +20,6 @@ export default class Home extends Component {
             <View title="Sunday" style={styles.content}>
               <Sessions />
             </View>
-
             {/* Second tab */}
             <View title="Monday" style={styles.content}>
               <Text style={styles.header}>Truly Native</Text>
